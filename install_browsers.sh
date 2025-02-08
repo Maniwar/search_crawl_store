729279877258326ca8602ebab780774994fdc6ab
Maniwar <mberenji@gmail.com>
2025-02-08 15:50:01 -0600

    Update install_browsers.sh

--- install_browsers.sh
+++ install_browsers.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# This script installs Playwright’s Chromium browser.
+# This script installs the Playwright Chromium browser.
+# NOTE: This may still fail if Node.js < 14 is installed.
+echo "Running install_browsers.sh ..."
 npx playwright install chromium
+echo "Done installing browsers!"
